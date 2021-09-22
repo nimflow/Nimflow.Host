14d38bb21416dceef1afd8c7a7f9860dd3e00491
Fabian Ramos <fabian@nimflow.com>
2021-09-22 19:15:17 +0200

    Fix portal public url and add missing /dist/ from nimflow-common-fns

--- Nimflow.Hub.WebApi/wwwroot/portal/__ENV.js
+++ Nimflow.Hub.WebApi/wwwroot/portal/__ENV.js
@@ -1,10 +1,1 @@
-window.__ENV =
-{
-     "REACT_APP_AUTH0_AUDIENCE": "https://nimflow.com/orchestration/api", 
-     "REACT_APP_AUTH0_CLIENT_ID": "hviuPQTq8xv2gNOSUWbX69tQEE6J3NU3", 
-     "REACT_APP_AUTH0_DOMAIN": "nimflow.us.auth0.com", 
-     "REACT_APP_NIMFLOW_BUSINESS_DIRECTORY_SERVER_URL": "..", 
-     "REACT_APP_PERSISTENT_CONFIG_KEY": "nimflowSeedConfiguration", 
-     "REACT_APP_PORTAL_URL": "https://portal.nimflow.com/", 
-     "REACT_APP_STUDIO_URL": "https://studio.nimflow.com/"
-};
+window.__ENV = {"REACT_APP_AUTH0_AUDIENCE":"https://nimflow.com/orchestration/api","REACT_APP_AUTH0_CLIENT_ID":"hviuPQTq8xv2gNOSUWbX69tQEE6J3NU3","REACT_APP_AUTH0_DOMAIN":"nimflow.us.auth0.com","REACT_APP_NIMFLOW_BUSINESS_DIRECTORY_SERVER_URL":"..","REACT_APP_PERSISTENT_CONFIG_KEY":"nimflowSeedConfiguration","REACT_APP_PORTAL_URL":"https://portal.nimflow.com/","REACT_APP_STUDIO_URL":"https://studio.nimflow.com/"};
