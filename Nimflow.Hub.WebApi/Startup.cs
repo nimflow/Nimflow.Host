22961c65cc0221c82b777a736d67406606418c68
Fabian Ramos <fabian@nimflow.com>
2021-10-13 10:59:17 +0200

    Disable SpaFileServer's cache for html and json files

--- Nimflow.Hub.WebApi/Startup.cs
+++ Nimflow.Hub.WebApi/Startup.cs
@@ -181,7 +181,18 @@ namespace Nimflow.Hub.WebApi
             var portalFsOptions = new FileServerOptions
             {
                 FileProvider = new PhysicalFileProvider(env?.WebRootPath != null ? Path.Combine(env.WebRootPath, name) : name),
-                RequestPath = $"/{name}"
+                RequestPath = $"/{name}",
+                StaticFileOptions =
+                {
+                    OnPrepareResponse = context =>
+                    {
+                        if (!context.File.Name.EndsWith("html") && !context.File.Name.EndsWith(".json"))
+                            return;
+                        context.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
+                        context.Context.Response.Headers["Pragma"] = "no-cache";
+                        context.Context.Response.Headers["Expires"] = "-1";
+                    }
+                }
             };
             if (defaultFileNames != null)
             {
